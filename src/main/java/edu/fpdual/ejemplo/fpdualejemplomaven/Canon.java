1c4696463829dbf75065bc0f15fc2d8034beb5fe
rafael.perez.castano <alum.rperezc.2021@iesalixar.org>
2022-04-08 13:41:22 +0200

    Commit maven con lombok

---
+++ src/main/java/edu/fpdual/ejemplo/fpdualejemplomaven/Canon.java
@@ -0,0 +1,13 @@
+package edu.fpdual.ejemplo.fpdualejemplomaven;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Canon {
+    private String letra;
+    private Integer frecuencia;
+
+}
---
+++ src/main/java/edu/fpdual/ejemplo/fpdualejemplomaven/Main.java
@@ -0,0 +1,7 @@
+package edu.fpdual.ejemplo.fpdualejemplomaven;
+
+public class Main {
+    public static void main(String[] args) {
+        Canon c1 = new Canon();
+    }
+}
